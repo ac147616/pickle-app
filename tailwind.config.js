66b40e5e4ab22a776a40aee269ae07bbdd9b32d6
Annika Singh <ac147616@avcol.school.nz>
2026-05-15 12:18:02 +1200

    configure tailwind with pickle brand colors and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green: {
+          dark: '#0c3120',
+        },
+        cream: '#f9e9da',
+      },
+      fontFamily: {
+        belleza: ['Belleza', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
-
